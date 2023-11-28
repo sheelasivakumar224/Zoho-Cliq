81179f4b6cc0a4ec6c0ca8208c8698a46f05d9d6
Sheela Sivakumar <125798345+sheelasivakumar224@users.noreply.github.com>
2023-11-28 21:58:56 +0530

    Create Generator.py

---
+++ Generator.py
@@ -0,0 +1,11 @@
+from flask import Flask, request,jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/',methods = ["GET"])
+def home():
+  return jsonify({'msg': 'Hello It worked'})
+
+if __name__ == ("__main__"):
+    app.run(debug= True)
